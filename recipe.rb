81ae4221ee38b06d8cc45e6502c131dc5e6660ce
hoshitocat <micky12127@gmail.com>
2015-06-06 14:39:13 +0900

    taks4 & task5 done

--- recipe.rb
+++ recipe.rb
@@ -1,2 +1,16 @@
-recipe_content = gets # IO#getsを使ってファイルの内容を取得
-p recipe_content
+# 配列をID付きで表示するメソッド
+def display_recipes
+  @recipes.each_with_index do |recipe, recipe_id|
+    puts "#{recipe_id.next}: #{recipe}"
+  end
+end
+
+@recipes = []
+
+# IO#getsを使ってファイルの内容を取得
+# 取得したファイルの中身を配列に格納する
+while recipe = gets
+  @recipes << recipe.chomp
+end
+
+display_recipes
